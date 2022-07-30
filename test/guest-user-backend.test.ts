7b1507f69247ad9869f7ee7a5b40595026a29850
Michael Liendo <mtliendo@gmail.com>
2022-07-30 12:07:39 -0500

    Initial commit

---
+++ test/guest-user-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GuestUserBackend from '../lib/guest-user-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/guest-user-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GuestUserBackend.GuestUserBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
